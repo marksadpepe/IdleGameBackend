f701e4c4e07b9bacdaebdddfabd51cc05b10fe73
Mark Parker <marksadpepe@gmail.com>
2024-10-26 23:19:54 +0300

    Configuration - added TypeORM configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,29 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { ConfigModule } from "@nestjs/config";
-import { serverConfig, databaseConfig } from "../configs";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import {
+  serverConfig,
+  databaseConfig,
+  getTypeormOptions,
+  DBConfigType,
+  DB_CONFIGURATION_KEY,
+} from "../configs";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [databaseConfig, serverConfig],
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) =>
+        getTypeormOptions(
+          config.getOrThrow<DBConfigType>(DB_CONFIGURATION_KEY),
+        ),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
