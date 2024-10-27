63e7335b05e9385f4c57022d7fc0b0efb7bb68b0
Mark Parker <marksadpepe@gmail.com>
2024-10-27 11:35:15 +0200

    added user entity

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+
+@Entity("users")
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({unique: true})
+  username: string
+
+  @Column()
+  password: string
+
+  @Column()
+  xp: number
+
+  @Column()
+  level: number
+
+  @Column({type: "timestamp", nullable: true})
+  last_seen_time: Date
+}
